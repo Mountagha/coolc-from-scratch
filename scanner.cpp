f04e00f86cec0147c8f2771012c699ad35bf3840
Mamadou Mountagha BAH <muntaghaba@gmail.com>
2023-09-13 03:13:37 -0400

    on Scanner

---
+++ scanner.cpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "scanner.hpp"
+#include "token.hpp"
+
+namespace cool {
+
+}
--- scanner.hpp
+++ scanner.hpp
@@ -15,7 +15,22 @@ class Scanner {
         unsigned int current;
         unsigned int line;
         const std::string& source;
+        std::vector<Token> tokens;
+
+        inline bool const isAtEnd() { return current > source.length(); }
+
+        std::vector<Token> scanToken() {
+            char c = advance()
+            switch(c) {
+                case '(': addToken()
+            } 
+        }
+
+        char advance() const { return source[current++]; }
+
+        void addToken(TokenType tokenType, std::string lexeme, unsigned int loc) {
+            tokens.push_back(Token(tokenType, lexeme, loc));
+        }
 
 };
-
-}
+} //
