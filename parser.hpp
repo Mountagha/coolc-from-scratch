0c577c2680dd7613908408d2697d13dd65d32846
Mountagha <muntaghaba@gmail.com>
2023-10-01 16:53:12 -0400

    now parsing...

--- parser.hpp
+++ parser.hpp
@@ -1,9 +1,30 @@
+#include "scanner.hpp"
+#include "ast.hpp"
+#include "scanner.hpp"
+
+
+#include <iostream> // debug purposes
+
 
 namespace cool {
 
 
 class Parser {
 // recursive top down.
+    public:
+        Parser(std::vector<Token>& tokens);
+        ~Parser();
+    
+    private:
+        Token current;
+        Program* program;
+
+        Expr* parseProgram() {
+            while (match(CLASS)) {
+
+            }
+        }
+
 };
 
 } // namespace cool
