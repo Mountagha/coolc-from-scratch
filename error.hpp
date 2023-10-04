1ce720bbb4e8dcbee02aeceacae2ee81509d7857
Mountagha <muntaghaba@gmail.com>
2023-10-04 08:01:20 -0400

    parsing cool

--- error.hpp
+++ error.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include "token.hpp"
 
 namespace cool {
 
@@ -10,6 +11,13 @@ static void error(int line, const std::string& msg) {
 static void fatal_error(int line, const std::string& msg) {
     error(line, msg);
     exit(1);
+}
+
+static void report(const Token& t, const std::string& msg) {
+    if (t.token_type == EOFILE)
+        error(t.loc, " at end", msg);
+    else 
+        error(t.loc, " at '" + t.lexeme + "'", msg);
 }
 
 
--- parser.hpp
+++ parser.hpp
@@ -1,6 +1,9 @@
+#pragma once
+
 #include "scanner.hpp"
 #include "ast.hpp"
 #include "scanner.hpp"
+#include "error.h"
 
 
 #include <iostream> // debug purposes
@@ -16,32 +19,61 @@ class Parser {
         ~Parser();
     
     private:
+        struct ParseError : std::runtime_error {
+            using std::runtime_error::runtime_error;
+        };
         std::vector<Token> tokens;
         unsigned int current;
         Program* program;
 
         Expr* parseProgram() {
-            while (match(CLASS)) {
+            while (match({CLASS})) {
+
 
             }
         }
 
-    bool match(const TokenType& t) const {
-        if (isAtEnd()) return false;
-        return peek().token_type == t;
-    }
+        bool check(const TokenType& t) const {
+            if (isAtEnd()) return false;
+            return peek().token_type == t;
+        }
+        
+        bool match(const std::vector<TokenType>& tts)  const {
+            for(auto& tokenType: tts){
+                if (peek().token_type == tokenType) return true;
+            }
+            return false;
+        }
 
-    bool isAtEnd() const {
-        return peek().token_type == EOFILE;
-    }
+        bool isAtEnd() const {
+            return peek().token_type == EOFILE;
+        }
 
-    Token peek() const {
-        return tokens[current];
-    }
+        Token peek() const {
+            return tokens[current];
+        }
 
-    Token previous() const {
-        return tokens[current - 1];
-    }
+        Token previous() const {
+            return tokens[current - 1];
+        }
+        inline Token consume(TokenType tt, const std::string& msg) {
+            if (check(tt)) return advance();
+
+            Token t = peek(); // cause of the passage by reference
+            throw error(t, msg);
+        }
+        Token advance() {
+            if(!isAtEnd()) current++;
+            return previous();
+        }
+
+        ParseError error(const Token& t, const std::string& msg) {
+            report(t, msg);
+
+            return ParseError{msg};
+        }
+
+
 
 };
 
