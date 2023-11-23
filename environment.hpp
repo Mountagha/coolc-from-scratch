2c9eddb44844e5077c1b596446053a1e4e190e4d
Mountagha <muntaghaba@gmail.com>
2023-11-22 23:44:46 -0500

    still on env

--- environment.hpp
+++ environment.hpp
@@ -3,6 +3,7 @@
 #include <unordered_map>
 #include <vector>
 #include <iostream>
+#include <list>
 
 namespace cool {
 
@@ -14,17 +15,21 @@ class Scope : public std::unordered_map<K, V> {
 template<class K, class V>
 class SymbolTable {
     private:
+        using ScopeList = std::list
         std::vector<Scope<K, V>> symtab{};
         size_t curr_scope;
     public:
         SymbolTable() {symtab.push_back({}); curr_scope(0);}
+
         void enterScope() {symtab.push_back({}), curr_scope++; };
+
         void exitScope() {
             if (curr_scope == 0) 
                 fatal_error("Cannot exit from the highest Scope.");
             curr_scope--;
         }
 
+        void addEntry()
         void fatal_error(const std::string& msg) {
             std::cerr << msg  << "\n";
             exit(1);
