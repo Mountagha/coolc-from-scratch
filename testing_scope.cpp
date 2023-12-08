2e0348050940f6b8879a5de9210b6b0cb60b1f6d
Mamadou Mountagha BAH <muntaghaba@gmail.com>
2023-12-08 07:44:29 -0500

    so that I can test on ubuntu side

--- testing_scope.cpp
+++ testing_scope.cpp
@@ -10,12 +10,19 @@ int main() {
     auto c = std::make_unique<int>(5);
     symTab.enterScope();
     symTab.insert(1, a.get());
-    auto ret = symTab.get(1);
-    std::cout << *ret; 
+    //symTab.enterScope();
     symTab.enterScope();
-    symTab.insert(2, b.get());
-    ret = symTab.get(1);
+    //symTab.enterScope();
+    //symTab.insert(1, a.get());
+    auto ret = symTab.get(1);
+    if (ret)
     std::cout << *ret; 
-    symTab.exitScope();
-    symTab.exitScope();
+    // symTab.enterScope();
+    // symTab.insert(2, b.get());
+    // ret = symTab.get(1);
+    // std::cout << *ret; 
+    // ret = symTab.get(2);
+    // std::cout << *ret; 
+    // symTab.exitScope();
+    // symTab.exitScope();
 }
