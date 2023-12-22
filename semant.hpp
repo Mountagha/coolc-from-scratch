550683d2c8bf8ce6deced1630e657df0e9dc4273
Mountagha <muntaghaba@gmail.com>
2023-12-22 13:11:37 -0500

    On semant still

--- semant.hpp
+++ semant.hpp
@@ -5,7 +5,6 @@
 #include <ostream>
 
 #include "token.hpp"
-#include "ast.hpp"
 #include "utilities.hpp"
 #include "environment.hpp"
 
@@ -19,71 +18,8 @@ namespace cool {
     as fixed names used by the runtime system.
 */
 
-IdTable idtable;
 
-static Token 
-    arg{TokenType::IDENTIFIER, "arg"},
-    arg2{TokenType::IDENTIFIER, "arg2"},
-    Bool{TokenType::IDENTIFIER, "Bool"},
-    concat{TokenType::IDENTIFIER, "concat"},
-    cool_abort{TokenType::IDENTIFIER, "cool_abort"},
-    copy{TokenType::IDENTIFIER, "copy"},
-    Int{TokenType::IDENTIFIER, "Int"},
-    in_int{TokenType::IDENTIFIER, "in_int"},
-    in_string{TokenType::IDENTIFIER, "in_string"},
-    IO{TokenType::IDENTIFIER, "IO"},
-    length{TokenType::IDENTIFIER, "length"},
-    Main{TokenType::IDENTIFIER, "Main"},
-    main_meth{TokenType::IDENTIFIER, "main_meth"},
-    No_class{TokenType::IDENTIFIER, "No_class"}, 
-    No_type{TokenType::IDENTIFIER, "No_type"},
-    Object{TokenType::IDENTIFIER, "Object"},
-    out_int{TokenType::IDENTIFIER, "out_int"},
-    out_string{TokenType::IDENTIFIER, "out_string"},
-    prim_slot{TokenType::IDENTIFIER, "prim_slot"},
-    self{TokenType::IDENTIFIER, "self"},
-    SELF_TYPE{TokenType::IDENTIFIER, "SELF_TYPE"},
-    Str{TokenType::IDENTIFIER, "Str"},
-    str_field{TokenType::IDENTIFIER, "str_field"},
-    substr{TokenType::IDENTIFIER, "substr"},
-    type_name{TokenType::IDENTIFIER, "type_name"},
-    val{TokenType::IDENTIFIER, "val"};
-
-static void initialize_constants() {
-    idtable.insert({"arg", arg});
-    idtable.insert({"arg2", arg2});
-    idtable.insert({"Bool", Bool});
-    idtable.insert({"concat", concat});
-    idtable.insert({"abort", cool_abort});
-    idtable.insert({"copy", copy});
-    idtable.insert({"Int", Int});
-    idtable.insert({"in_int", in_int});
-    idtable.insert({"in_string", in_string});
-    idtable.insert({"IO", IO});
-    idtable.insert({"length", length});
-    idtable.insert({"Main", Main});
-    idtable.insert({"main", main_meth});
-
-    //  _no_class is a symbol that can't be the name of
-    // any user_defined class.
-
-    idtable.insert({"_no_class", No_class});
-    idtable.insert({"_no_type", No_type});
-    idtable.insert({"Object", Object});
-    idtable.insert({"out_int", out_int});
-    idtable.insert({"out_string", out_string});
-    idtable.insert({"_prim_slot", prim_slot});
-    idtable.insert({"self", self});
-    idtable.insert({"SELF_TYPE", SELF_TYPE});
-    idtable.insert({"String", Str});
-    idtable.insert({"_str_field", str_field});
-    idtable.insert({"substr", substr});
-    idtable.insert({"type_name", type_name});
-    idtable.insert({"_val", val});
-
-}
-
-Class curr_class; // Not fan of global. But will figure out later.
+// Class curr_class; // Not fan of global. But will figure out later.
 
 class Semant : public StmtVisitor, public ExprVisitor {
     public:
@@ -108,7 +44,7 @@ class Semant : public StmtVisitor, public ExprVisitor {
                 class_->accept(this);
             }
 
-            check_parents(stmt);
+            check_inheritance(stmt);
             
         }
 
@@ -188,8 +124,34 @@ class Semant : public StmtVisitor, public ExprVisitor {
             }
             return ret;
         }
-        void check_inheritance(Program* stmt) {
 
+        bool check_DAG(Program* stmt) {
+            
+            bool ret = true;
+            // Build the inheritance_graph
+
+            InheritanceGraph g;
+            g.addEdge(IO, Object);
+            g.addEdge(Int, Object);
+            g.addEdge(Str, Object);
+            g.addEdge(Bool, Object);
+
+            for (auto& class_: stmt->classes) {
+                g.addEdge(class_->name, class_->superClass->name);
+            }
+
+            if(!g.isDGA()) {
+                semant_error(curr_class.name, "Cyclic inheritance detected!"); // find better way to find the error position.
+                ret = false;
+            }
+            return ret;
+        }
+        void check_inheritance(Program* stmt) {
+            // check if parents are defined.
+            if (!check_parents(stmt)) return;
+
+            // check if the graph does not contains cycle. 
+            check_DAG(stmt);
         }
 
         
