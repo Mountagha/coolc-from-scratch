882d15a3499f38ae9500b7066f40b61761aeef95
Mountagha <muntaghaba@gmail.com>
2024-02-07 10:09:49 -0500

    almost donnnne

---
+++ build/CMakeFiles/coolc.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mountagha/coolc-from-scratch/ASTPrinter.cpp" "/home/mountagha/coolc-from-scratch/build/CMakeFiles/coolc.dir/ASTPrinter.cpp.o"
+  "/home/mountagha/coolc-from-scratch/ast.cpp" "/home/mountagha/coolc-from-scratch/build/CMakeFiles/coolc.dir/ast.cpp.o"
+  "/home/mountagha/coolc-from-scratch/environment.cpp" "/home/mountagha/coolc-from-scratch/build/CMakeFiles/coolc.dir/environment.cpp.o"
+  "/home/mountagha/coolc-from-scratch/main.cpp" "/home/mountagha/coolc-from-scratch/build/CMakeFiles/coolc.dir/main.cpp.o"
+  "/home/mountagha/coolc-from-scratch/object.cpp" "/home/mountagha/coolc-from-scratch/build/CMakeFiles/coolc.dir/object.cpp.o"
+  "/home/mountagha/coolc-from-scratch/parser.cpp" "/home/mountagha/coolc-from-scratch/build/CMakeFiles/coolc.dir/parser.cpp.o"
+  "/home/mountagha/coolc-from-scratch/scanner.cpp" "/home/mountagha/coolc-from-scratch/build/CMakeFiles/coolc.dir/scanner.cpp.o"
+  "/home/mountagha/coolc-from-scratch/semant.cpp" "/home/mountagha/coolc-from-scratch/build/CMakeFiles/coolc.dir/semant.cpp.o"
+  "/home/mountagha/coolc-from-scratch/token.cpp" "/home/mountagha/coolc-from-scratch/build/CMakeFiles/coolc.dir/token.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
