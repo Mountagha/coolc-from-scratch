7dd7657154a4a5689bbaf886496ffb8e97abf206
Mountagha <muntaghaba@gmail.com>
2024-06-08 15:21:28 -0400

    All working fine. Now need refactor and better errors handling

--- out.s
+++ out.s
@@ -28,58 +28,149 @@ str_const8:
 	.word	7
 	.word	4
 	.word	String_dispTab
-	.word	0
+	.word	int_const10
 	.ascii	""
+	.byte	0
 	.align	2
+	.word	-1
 str_const2:
 	.word	7
 	.word	5
 	.word	String_dispTab
-	.word	4
+	.word	int_const2
 	.ascii	"Bool"
+	.byte	0
 	.align	2
+	.word	-1
+str_const1:
+	.word	7
+	.word	8
+	.word	String_dispTab
+	.word	int_const7
+	.ascii	"Hello, World.\n"
+	.byte	0
+	.align	2
+	.word	-1
 str_const3:
 	.word	7
-	.word	4
+	.word	5
 	.word	String_dispTab
-	.word	2
+	.word	int_const4
 	.ascii	"IO"
+	.byte	0
 	.align	2
+	.word	-1
 str_const4:
 	.word	7
-	.word	4
+	.word	5
 	.word	String_dispTab
-	.word	3
+	.word	int_const8
 	.ascii	"Int"
+	.byte	0
 	.align	2
+	.word	-1
 str_const5:
 	.word	7
 	.word	5
 	.word	String_dispTab
-	.word	4
+	.word	int_const2
 	.ascii	"Main"
+	.byte	0
 	.align	2
+	.word	-1
 str_const6:
 	.word	7
-	.word	5
-	.word	String_dispTab
 	.word	6
+	.word	String_dispTab
+	.word	int_const9
 	.ascii	"Object"
+	.byte	0
 	.align	2
+	.word	-1
 str_const7:
 	.word	7
-	.word	5
-	.word	String_dispTab
 	.word	6
+	.word	String_dispTab
+	.word	int_const9
 	.ascii	"String"
+	.byte	0
 	.align	2
-str_const1:
+	.word	-1
+str_const0:
 	.word	7
-	.word	6
+	.word	8
 	.word	String_dispTab
-	.word	10
-	.ascii	"hellowww\n"
+	.word	int_const0
+	.ascii	"hello_world.cl"
+	.byte	0
 	.align	2
+	.word	-1
+int_const10:
+	.word	6
+	.word	4
+	.word	Int_dispTab
+	.word	0
+	.word	-1
+int_const6:
+	.word	6
+	.word	4
+	.word	Int_dispTab
+	.word	10
+	.word	-1
+int_const0:
+	.word	6
+	.word	4
+	.word	Int_dispTab
+	.word	14
+	.word	-1
+int_const7:
+	.word	6
+	.word	4
+	.word	Int_dispTab
+	.word	15
+	.word	-1
+int_const4:
+	.word	6
+	.word	4
+	.word	Int_dispTab
+	.word	2
+	.word	-1
+int_const8:
+	.word	6
+	.word	4
+	.word	Int_dispTab
+	.word	3
+	.word	-1
+int_const2:
+	.word	6
+	.word	4
+	.word	Int_dispTab
+	.word	4
+	.word	-1
+int_const1:
+	.word	6
+	.word	4
+	.word	Int_dispTab
+	.word	5
+	.word	-1
+int_const9:
+	.word	6
+	.word	4
+	.word	Int_dispTab
+	.word	6
+	.word	-1
+int_const3:
+	.word	6
+	.word	4
+	.word	Int_dispTab
+	.word	8
+	.word	-1
+int_const5:
+	.word	6
+	.word	4
+	.word	Int_dispTab
+	.word	9
+	.word	-1
 bool_const0:
 	.word	5
 	.word	4
@@ -91,12 +182,13 @@ bool_const1:
 	.word	Bool_dispTab
 	.word	1
 class_nameTab:
-	.word	str_const2
-	.word	str_const3
-	.word	str_const4
-	.word	str_const5
+	.space	16
 	.word	str_const6
+	.word	str_const2
+	.word	str_const4
 	.word	str_const7
+	.word	str_const3
+	.word	str_const5
 Bool_dispTab:
 	.word	Object.abort
 	.word	Object.type_name
@@ -133,15 +225,14 @@ String_dispTab:
 	.word	String.length
 	.word	String.concat
 	.word	String.substr
-Bool_protObj:
-	.word	5
-	.word	4
-	.word	Bool_dispTab
-	.word	0
 IO_protObj:
 	.word	8
 	.word	3
 	.word	IO_dispTab
+Object_protObj:
+	.word	4
+	.word	3
+	.word	Object_dispTab
 Int_protObj:
 	.word	6
 	.word	4
@@ -151,15 +242,16 @@ Main_protObj:
 	.word	9
 	.word	3
 	.word	Main_dispTab
-Object_protObj:
-	.word	10
-	.word	3
-	.word	Object_dispTab
 String_protObj:
 	.word	7
 	.word	5
 	.word	String_dispTab
 	.word	0
+	.word	0
+Bool_protObj:
+	.word	5
+	.word	4
+	.word	Bool_dispTab
 	.word	0
 	.globl	heap_start
 heap_start:
@@ -205,6 +297,7 @@ String_init:
 	addiu	$fp, $sp, 4
 	move	$s0, $a0
 	jal	Object_init
+	la	$a0, int_const10
 	sw	$a0, 12($s0)
 	move	$a0, $s0
 	lw	$fp, 12($sp)
@@ -255,19 +348,28 @@ Main_init:
 	addiu	$sp, $sp, 12
 	jr	$ra
 Main.main:
+	addiu	$sp, $sp, -12
+	sw	$fp, 12($sp)
+	sw	$s0, 8($sp)
 	sw	$ra, 4($sp)
+	move	$s0, $a0
 	addiu	$sp, $sp, -16
 	sw	$fp, 16($sp)
 	sw	$s0, 12($sp)
 	la	$a0, str_const1
 	sw	$a0, 8($sp)
-	addiu	$fp, $sp, 4
 	move	$a0, $s0
+	addiu	$fp, $sp, 4
+	bne	$a0, $zero, DispatchLabel0
+	la	$a0, str_const0
+	li	$t1, 1
+	jal	_dispatch_abort
+DispatchLabel0:
 	lw	$t1, 8($a0)
-	lw	$t1, 0($t1)
+	lw	$t1, 12($t1)
 	jalr	$t1
 	lw	$fp, 12($sp)
 	lw	$s0, 8($sp)
-	lw	$ra, 0($sp)
+	lw	$ra, 4($sp)
 	addiu	$sp, $sp, 12
 	jr	$ra
